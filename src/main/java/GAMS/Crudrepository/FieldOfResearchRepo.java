9a8b628297b8b6b7ad19f9e642679ae2e2c42e99
zohamehdi <43473034+zohamehdi@users.noreply.github.com>
2020-03-05 13:43:35 -0500

    Field Of Research Repository

---
+++ src/main/java/GAMS/Crudrepository/FieldOfResearchRepo.java
@@ -0,0 +1,13 @@
+package GAMS.Crudrepository;
+
+import GAMS.entity.FieldOfResearch;
+import GAMS.entity.Professor;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FieldOfResearchRepo extends CrudRepository<FieldOfResearch, String> {
+    FieldOfResearch findById(long id);
+    FieldOfResearch findByName(String name);
+    FieldOfResearch findByProfessor(Professor professor);
+}

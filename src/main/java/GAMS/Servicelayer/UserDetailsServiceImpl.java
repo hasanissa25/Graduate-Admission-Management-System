52df62b821fb71ab597c09edd180631f8afd44c1
Hasan Issa <hasan.issa@carleton.ca>
2020-03-06 11:30:52 -0500

    Fixed some smelly code

--- src/main/java/GAMS/Servicelayer/UserDetailsServiceImpl.java
+++ src/main/java/GAMS/Servicelayer/UserDetailsServiceImpl.java
@@ -15,6 +15,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+
 @Service("userDetailsService")
 @Transactional
 public class UserDetailsServiceImpl implements UserDetailsService{

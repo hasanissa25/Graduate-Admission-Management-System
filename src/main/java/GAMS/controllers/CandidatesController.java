07556dd94c7983772d0a81a6435bd7beea788bfe
tashfiqakhand <TashfiqAkhand@cmail.carleton.ca>
2020-03-28 15:38:41 -0400

    Student candidates page completed

--- src/main/java/GAMS/controllers/CandidatesController.java
+++ src/main/java/GAMS/controllers/CandidatesController.java
@@ -9,10 +9,14 @@ import GAMS.entity.Student;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import java.util.*;
 
+@Controller
 public class CandidatesController {
 
     @Autowired
@@ -23,20 +27,21 @@ public class CandidatesController {
 
 
     @GetMapping("/StudentCandidates")
-    public String list(Model model) {
+    public String ListOfCandidates(Model model) {
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+
         Iterable<FieldOfResearch> all = fieldOfResearchRepo.findAll();
         Iterable<Student> all2 = studentRepo.findAll();
-
         List<FieldOfResearch> active = new ArrayList<>();
+
         for(FieldOfResearch research : all){
             if(research.isActive()){
                 active.add(research);
             }
         }
-        model.addAttribute("tash", active);
+        model.addAttribute("dat", active);
 
-        model.addAttribute("dat", all2);
+        model.addAttribute("dat2", all2);
 
         model.addAttribute("view", "StudentCandidates");
         return "layout";

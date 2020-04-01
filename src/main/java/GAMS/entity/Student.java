807e0014383245567292ba2c3119c292202d998d
Hasan Issa <hasan.issa@carleton.ca>
2020-04-01 13:55:42 -0400

    Added User name into the index page Increased time out on the email service

--- src/main/java/GAMS/entity/Student.java
+++ src/main/java/GAMS/entity/Student.java
@@ -14,7 +14,11 @@ import java.util.ArrayList;
 public class Student extends EndUser implements Serializable {
 
 
-
+    @Getter
+    @Setter
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
 
     private String email;
@@ -72,5 +76,13 @@ public class Student extends EndUser implements Serializable {
         return cv;
     }
 
-  
+    @Override
+    public Long getId() {
+        return id;
+    }
+
+    @Override
+    public void setId(Long id) {
+        this.id = id;
+    }
 }
